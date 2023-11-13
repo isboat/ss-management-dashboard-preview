fcd22195d73f785a9c2f4a1e6ee43b8a73086c8a
isaac boateng <isboateng@hotmail.com>
2023-11-13 18:55:40 +0000

    update

--- src/ss-management-dashboard-preview/src/app/data.model.ts
+++ src/ss-management-dashboard-preview/src/app/data.model.ts
@@ -1,8 +1,27 @@
-export class DataResponse
-{
-    created: string;
-    modifiedDate: string;
-    id: string;
-    tenantId: string;
-    name: string;
+export class DataResponse {
+    displayName: string
+    externalMediaSource: string
+    tenantId: string
+    id: string
+    mediaAsset: MediaAsset
+    mediaAssetEntityId: string
+    menu: MenuModel
+    menuEntityId: string
+    templateKey: string
+    templateProperties: TemplateProperty[]
+}
+export class TemplateProperty {
+    key: string
+    value: string
+    label: string
+}
+
+export class MediaAsset {
+    tenantId: string
+    id: string
+}
+
+export class MenuModel {
+    tenantId: string
+    id: string
 }
